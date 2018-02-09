4d326fb461f871d69a5a73da326809606a7668ab
tonney7 <30279093+tonney7@users.noreply.github.com>
2018-02-08 23:16:18 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    char n;
+    node * next;
+};
+
+class Pig_Latin_linked_List{
+private:
+    node * head;
+public:
+    Pig_Latin_linked_List();
+
+    void add(char character){
+        node * cu = head;
+        
+        //if first node in system
+        if (cu == 0){
+            cu = new node;
+            cu->n = character;
+            cu->next = 0;
+            return;
+        }
+        
+        while (cu->next != 0) {
+            cu = cu->next;
+        }
+        cu->next = new node;
+        cu = cu->next;
+        cu->n = character;
+        cu->next = 0;
+    }
+    
+    
+    void print(){
+        node * cu = head;
+        while (cu->next != 0){
+            cout<<cu->n;
+            cu= cu->next;
+        }
+    }
+    
+};
+
+int main() {
+    cout<<"Please Enter in 1 word at a time (ie no spaces):"<<endl;
+    
+    bool done = false;
+    string Word;
+    string StopPhrase = "Qwerty";
+    Pig_Latin_linked_List LINKEDLIST;
+    
+    while (!done){
+        cout<<"What is your word (to stop, type in Qwerty)"<<endl;
+        cin>>Word;
+        // if stoping word
+        if (Word == StopPhrase){
+            done = true;
+            continue;
+        }
+        for (int i = 0; i < Word.size();i++){
+            LINKEDLIST.add(Word[i]);
+        }
+  
+    }
+    
+    cout<<endl<<endl<<endl;
+    LINKEDLIST.print();
+    cout<<"\nDONE"<<endl;
+    
+    
+    
+    
+    
+    
+    
+    /*
+    //trying out Linked List
+    node * head = new node;
+    head->n = 0;
+    node * cu = head;
+    for (int i = 1; i < 11;i++){
+        cu->next = new node;
+        cu = cu->next;
+        cu->n = i;
+    }
+    
+    cu = head;
+    for (int i = 0; i < 11;i++){
+        cout<<cu->n<<" ";
+        cu = cu->next;
+    }
+    cout<<endl;
+     */
+    return 0;
+}
